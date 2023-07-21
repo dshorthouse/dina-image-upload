526f7ee6f3bae1030392afab787244a15f0b6d61
David Shorthouse <davidpshorthouse@gmail.com>
2023-07-21 12:41:57 -0400

    Use block for transactions in db

--- database/database.rb
+++ database/database.rb
@@ -5,32 +5,32 @@ class Database
   end
 
   def create_schema
-    @db.transaction
-    @db.execute "DROP TABLE IF EXISTS directories"
-    @db.execute "DROP TABLE IF EXISTS logs"
-    @db.execute "DROP TABLE IF EXISTS errors"
+    @db.transaction do
+      @db.execute "DROP TABLE IF EXISTS directories"
+      @db.execute "DROP TABLE IF EXISTS logs"
+      @db.execute "DROP TABLE IF EXISTS errors"
 
-    @db.execute <<-SQL
-      CREATE TABLE directories (
-        directory varchar(256)
-      );
-    SQL
-    @db.execute <<-SQL
-      CREATE TABLE logs (
-        directory varchar(256),
-        object char(36),
-        derivative char(36),
-        image_original char(36),
-        image_derivative char(36)
-      );
-    SQL
-    @db.execute <<-SQL
-      CREATE TABLE errors (
-        directory varchar(256),
-        type varchar(256)
-      );
-    SQL
-    @db.commit
+      @db.execute <<-SQL
+        CREATE TABLE directories (
+          directory varchar(256)
+        );
+      SQL
+      @db.execute <<-SQL
+        CREATE TABLE logs (
+          directory varchar(256),
+          object char(36),
+          derivative char(36),
+          image_original char(36),
+          image_derivative char(36)
+        );
+      SQL
+      @db.execute <<-SQL
+        CREATE TABLE errors (
+          directory varchar(256),
+          type varchar(256)
+        );
+      SQL
+    end
   end
 
   def insert(table:, hash:)
@@ -44,9 +44,9 @@ class Database
   end
 
   def delete_directory(rowid:)
-    @db.transaction
-    @db.execute "DELETE FROM directories WHERE rowid = ?", rowid
-    @db.commit
+    @db.transaction do
+      @db.execute "DELETE FROM directories WHERE rowid = ?", rowid
+    end
   end
 
   def select_max_directory_rowid
@@ -54,18 +54,18 @@ class Database
   end
 
   def update_directories_rowid
-    @db.transaction
-    @db.execute "CREATE TABLE directories_tmp (directory varchar(256))"
-    @db.execute "INSERT INTO directories_tmp SELECT directory FROM directories"
-    @db.execute "DROP TABLE IF EXISTS directories"
-    @db.execute "ALTER TABLE directories_tmp RENAME TO directories"
-    @db.commit
+    @db.transaction do
+      @db.execute "CREATE TABLE directories_tmp (directory varchar(256))"
+      @db.execute "INSERT INTO directories_tmp SELECT directory FROM directories"
+      @db.execute "DROP TABLE IF EXISTS directories"
+      @db.execute "ALTER TABLE directories_tmp RENAME TO directories"
+    end
   end
 
   def truncate_directories
-    @db.transaction
-    @db.execute "DELETE FROM directories"
-    @db.commit
+    @db.transaction do
+      @db.execute "DELETE FROM directories"
+    end
   end
 
 end
